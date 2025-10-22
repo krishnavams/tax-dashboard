5028f3d9fbc4165d04fd60775aecaed4ff41d299
krishnavams01~ <vamsi.krishna766@gmail.com>
2025-10-22 17:00:45 +0530

    adding first commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 import data from "./data.json";
+import { SectionCards } from "@/components/section-cards";
 
 export default function Page() {
   return (
@@ -22,6 +23,9 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <SectionCards />
+              <SectionCards />
               <div className="px-4 lg:px-6">
                 <ChartAreaInteractive />
               </div>
--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -10,93 +10,52 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
+interface TaxCardValues {
+  title: string;
+  value: string;
+  change: string;
+  trendText: string;
+  percentageIndicator: string;
+  isPositive?: boolean;
+}
+
+export function StatCard({title, value, change, trendText, percentageIndicator, isPositive}:TaxCardValues){
+  return (
+  <>
+  <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>{title}</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            {value}
+          </CardTitle>
+          <CardAction>
+            <Badge variant="outline">
+              {isPositive ? (<IconTrendingUp />) : (<IconTrendingDown />)}
+              {percentageIndicator}%
+            </Badge>
+          </CardAction>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+          <div className="line-clamp-1 flex gap-2 font-medium">
+            {change} 
+            {isPositive ? (<IconTrendingUp className="size-4" />) : (<IconTrendingDown className="size-4"/>)}
+          </div>
+          <div className="text-muted-foreground">
+            {trendText}
+          </div>
+        </CardFooter>
+      </Card>
+      </>)
+
+}
+
 export function SectionCards() {
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>New Customers</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingDown />
-              -20%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <IconTrendingDown className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Acquisition needs attention
-          </div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Active Accounts</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            45,678
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Growth Rate</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            4.5%
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +4.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
-        </CardFooter>
-      </Card>
+      <StatCard title="Tax Amount" value="₹1,250.00" change="Trending up this month" trendText="Visitors for the last 6 months" percentageIndicator="12.50" isPositive={true}/>
+      <StatCard title="Tax Volume" value="1,250" change="Down 20% this period" trendText="Acquisition needs attention" percentageIndicator="20" isPositive={false}/>
+      <StatCard title="Commission" value="₹91,250" change="Strong user retention" trendText="Engagement exceed targets" percentageIndicator="12.50" isPositive={true}/>
+      <StatCard title="Growth Rate" value="4.5%" change="Steady performance increase" trendText="Meets growth projections" percentageIndicator="12.50" isPositive={true}/>
     </div>
   )
 }
